32b985d3d30c36da2381749be2817c62a197104e
saiteja-in <vurukondasaiteja13@gmail.com>
2025-12-22 19:13:53 +0530

    header added in page

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -1,11 +1,66 @@
-import React from 'react'
+"use client";
+import { useParams } from "next/navigation";
+import React, { useState } from "react";
 
-const page = () => {
-  return (
-    <div>
-      hey there
-    </div>
-  )
+function formatTimeRemaining(seconds: number) {
+  const mins = Math.floor(seconds / 60);
+  const secs = seconds % 60;
+  return `${mins}:${secs.toString().padStart(2, "0")}`;
 }
 
-export default page
+const Page = () => {
+  const params = useParams();
+  const roomId = params.roomId as string;
+
+  const [copyStatus, setCopyStatus] = useState("COPY");
+  const [timeRemaining, setTimeRemaining] = useState<number | null>(121);
+  const copyLink = () => {
+    const url = window.location.href;
+    navigator.clipboard.writeText(url);
+    setCopyStatus("COPIED");
+    setTimeout(() => setCopyStatus("COPY"), 1500);
+  };
+  return (
+    <main className="flex flex-col h-screen max-h-screen overflow-hidden">
+      <header className="border-b border-zinc-800 p-4 flex items-center justify-between bg-zinc-900/30">
+        <div className="flex items-center gap-4">
+          <div className="flex flex-col">
+            <span className="text-xs text-zinc-500 uppercase">Room Id</span>
+            <div className="flex items-center gap-2">
+              <span className="font-bold text-green-500">{roomId}</span>
+              <button
+                onClick={() => copyLink()}
+                className="text-[10px] bg-zinc-800 hover:bg-zinc-700 px-2 py-0.5 rounded text-zinc-400 hover:text-zinc-200 transition-colors"
+              >
+                {copyStatus}
+              </button>
+            </div>
+          </div>
+          <div className="h-8 w-px bg-zinc-800" />
+          <div className="flex flex-col">
+            <span className="text-xs text-zinc-500 uppercase">
+              self-destruct
+            </span>
+            <span
+              className={`text-sm font-bold flex items-center gap-2 ${
+                timeRemaining !== null && timeRemaining < 60
+                  ? "text-red-500"
+                  : "text-amber-500"
+              }`}
+            >
+              {timeRemaining !== null
+                ? formatTimeRemaining(timeRemaining)
+                : "--:--"}
+            </span>
+          </div>
+        </div>
+        <button className="text-xs bg-zinc-800 hover:bg-red-600 px-3 py-1.5 rounded text-zinc-400 hover:text-white font-bold transition-all group flex items-center gap-2 disabled:opacity-50 uppercase">
+          <span className="group-hover:animate-pulse">☠️</span>
+          destroy now
+        </button>
+      </header>
+    </main>
+  );
+};
+
+export default Page;
