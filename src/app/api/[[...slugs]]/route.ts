7279386e5e604ec79b958a951e45dc8ae36b1950
saiteja-in <vurukondasaiteja13@gmail.com>
2025-12-23 00:25:26 +0530

    auth middleware by elysia done

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,22 +1,39 @@
 import { redis } from "@/lib/redis";
 import { Elysia, t } from "elysia";
 import { nanoid } from "nanoid";
+import { authMiddleware } from "./auth";
+import z from "zod";
 
-const ROOM_TTL_SECONDS = 60*10;
-const rooms = new Elysia({prefix:"/room"}).
-post("/create",async()=>{
-    const roomId = nanoid()
-    await redis.hset(`meta:${roomId}`,{
-        connected:[],
-        createdAt:Date.now()
-    })
-    await redis.expire(`meta:${roomId}`,ROOM_TTL_SECONDS)
-    console.log("room creation")
-    return {roomId}
-})
+const ROOM_TTL_SECONDS = 60 * 10;
+const rooms = new Elysia({ prefix: "/room" }).post("/create", async () => {
+  const roomId = nanoid();
+  await redis.hset(`meta:${roomId}`, {
+    connected: [],
+    createdAt: Date.now(),
+  });
+  await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS);
+  console.log("room creation");
+  return { roomId };
+});
+const messages = new Elysia({ prefix: "/messages" }).use(authMiddleware).post(
+  "/",
+  async ({ body, auth }) => {
+    const { sender, text } = body;
+    const {roomId}=auth;
+    const roomExists = await redis.exists(`meta:${roomId}`)
+    if(!roomExists)throw new Error("Room does not exist")
+    
+  },
+  {
+    query: z.object({ roomId: z.string() }),
+    body: z.object({
+      sender: z.string().max(100),
+      text: z.string().max(1000),
+    }),
+  }
+);
 
-
-export const app = new Elysia({ prefix: "/api" }).use(rooms)
+export const app = new Elysia({ prefix: "/api" }).use(rooms).use(messages);
 
 export const GET = app.fetch;
 export const POST = app.fetch;
