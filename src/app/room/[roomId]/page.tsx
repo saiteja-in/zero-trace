411eae44d5d0cbd333ff89ede2970a9b660e5179
saiteja-in <vurukondasaiteja13@gmail.com>
2025-12-22 22:57:25 +0530

    cool looking input and button

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -59,6 +59,24 @@ const Page = () => {
           destroy now
         </button>
       </header>
+      <div className="flex-1 overflow-y-auto p-4 space-y-4 scrollbar-thin"></div>
+      <div className="p-4 border-t border-zinc-800 bg-zinc-900/30">
+        <div className="flex gap-4">
+          <div className="flex-1 relative group">
+            <span className="absolute left-4 top-1/2 -translate-1/2 text-green-500 animate-pulse">
+              {">"}
+            </span>
+            <input
+              autoFocus
+              type="text"
+              className="w-full bg-black border border-zinc-800 focus:border-zinc-700 focus:outline-none transition-colors text-zinc-100 placeholder:text-zinc-700 py-3 pl-8 pr-4 text-sm"
+            />
+          </div>
+          <button className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:cursor-not-allowed disabled:opacity-50 cursor-pointer">
+            SEND
+          </button>
+        </div>
+      </div>
     </main>
   );
 };
